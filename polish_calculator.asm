025f299cb0c5eeb1f50a32ed890c4683f4255f8f
louis <louis.milhaud@universite-paris-saclay.fr>
2023-09-26 23:51:40 +0200

    rethink the calculator

--- polish_calculator.asm
+++ polish_calculator.asm
@@ -1,85 +1,80 @@
-.data
-	result:        .word	0
-	input_buffer:  .word	0
-	
-	start_stack:  .byte	0
-	
-	str_begin:   .asciiz 	"\nHello, this is a simple Polish calculator.\n"
-	str_input:   .asciiz	"\nenter integer or operation (+, -): "
-	print_char:  .asciiz	"p"
-	add_char:    .asciiz	"+"
-	sub_char:    .asciiz	"-"
-	str_end:     .asciiz	"\nHere is the result: "
-	custom_text: .asciiz"\nyou entered an integer"
+	.data
+input_buffer: .space 45
+str_begin:   .asciiz 	"\nHello, this is a simple Polish calculator.\n"
+str_input:   .asciiz	"\nenter integer or operation (+): "
+print_char:  .asciiz	'p'
+add_char:    .asciiz	'+'
+sub_char:    .asciiz	'-'
+str_end:     .asciiz	"\nHere is the result: "
+custom_text: .asciiz	"\nyou entered an integer"
 
-.text 
-	li $t0, 20
-	li $t1, 3
-	la $t0, ($sp)
-	sw $t0, start_stack
+	.text
+	#- $t0 pour le res
+	#- $t1 pour l'adresse du string
+	#- $t2 le cpt pour savoir si string fini
+	#- $t3 l'adresse du debut de la stack
+	#- $t4-$t7 pour load des bits et effectuer calculs depuis 
+	
+	li $t0, 0
+	la $t3, 0($sp)
 		
 	# printing presentation
-	pres:
-		la $a0, str_begin
-		li $v0, 4
-		syscall
+pres:
+	la $a0, str_begin
+	li $v0, 4
+	syscall
 		
-	# asking for input
-	ask_input:
-		la $a0, str_input
-		li $v0, 4
-		syscall
-	
-	# reading input
-	read_input:
-		# read user input
-		la $a0, input_buffer
-		li $a1, 3
-		li $v0, 8
-		syscall
+	# asking for input then save it
+input:
+	la $a0, str_input
+	li $v0, 4
+	syscall
 
-		#check if input == p
-		la $t0, input_buffer
-		lb $t1, ($t0)
-		la $t2, print_char
-		lb $t2, ($t2)
-		beq $t1, $t2, end
+	la $a0, input_buffer
+	li $a1, 3
+	li $v0, 8
+	syscall
+	la $t1, input_buffer #load input adress in reg $t1
+	li $t2, 0 #start cpt
+	
+load_char:
+	lb $t4, ($t1)
+	addi $t1, $t1, 1
+	addi $t2, $t2, 1
+	
+	#check if string is done (TODO: change 40)
+	beq $t2, 40, input
+
+	#check if input == p
+	la $t5, print_char
+	lb $t5, ($t5)
+	beq $t4, $t5, end
 		
-		# if not p, check if operation (TODO: add substraction)
-		la $t2, add_char
-		lb $t2, ($t2)
-		beq $t1, $t2, end
+	# if not p, check if add
+	la $t5, add_char
+	lb $t5, ($t5)
+	beq $t4, $t5, addition
 		
-		# if not operation, then stack the number
-		lw $t1, ($t0)
-		sw $t1, ($sp)
-		sub $sp, $sp, 4
-		j custom
-		#j read_input
+	# if not add, then transform to int and stack the number
+	# TODO: transform to int
+	sw $t4, ($sp)
+	sub $sp, $sp, 4
+	j load_char
 		
-	# for test purposes
-	custom:	
-		la $a0, custom_text
-		li $v0, 8
-		syscall
-		j end
-		
-	addition:
-		# add every stack element to result
-		lw $t0, start_stack
-		beq $sp, $t0, end # if stack empty
-		lw $t0, ($sp)
-		sub $sp, $sp, 4
-		lw $t1, result
-		add $t1, $t1, $t0
-		sw $t1, result
-		
-	end:
-		# print end message
-		la $a0, str_end
-		li $v0, 4
-		syscall
-		# print result
-		lw $a0, result
-		li $v0, 1
-		syscall
+addition:
+	# add every stack element to result
+	beq $sp, $t3, end #if stack empty: stop
+	lw $t4, ($sp)
+	addi $sp, $sp, 4
+	add $t0, $t4, $t0
+	j addition
+	
+end:
+	# print end message
+	la $a0, str_end
+	li $v0, 4
+	syscall
+	# print result
+	move $a0, $t0
+	li $v0, 1
+	syscall
