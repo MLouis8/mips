a3d3718e8665da4e382b7caec6944446867a09d4
louis <louis.milhaud@universite-paris-saclay.fr>
2023-09-27 10:11:24 +0200

    strange polish calculator done

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
 # mips
-Pour la calculatrice:
-
-- $t0 pour le res
-- $t1 pour l'adresse du string
-- $t2 le cpt pour savoir si string fini
-- $t3 l'adresse de la fin de la stack
-- $t4-$t7 pour load des bits et effectuer calculs depuis la stack
-1) fonction qui demande et stock le string dans la memoire, garde l'adresse en $t1
-2) fonction qui lit bit a bit et si
-    - si "p" alors print le res
-    - si op alors appelle fonction de calcul correspondante
-    - si n alors le transformer en int et le stocker dans la pile
-2bis) calcul jusqu'a la "fin" de la pile en fonction de op
-
-Plus tard: changer le 2) en rajoutant les espaces pour avoir en dessus de 9
+Calculatrice Polonaise cheloue.
--- polish_calculator.asm
+++ polish_calculator.asm
@@ -1,10 +1,11 @@
 	.data
-input_buffer: .space 45
+input_buffer: .space 11
 str_begin:   .asciiz 	"\nHello, this is a simple Polish calculator.\n"
-str_input:   .asciiz	"\nenter integer or operation (+): "
-print_char:  .asciiz	'p'
-add_char:    .asciiz	'+'
-sub_char:    .asciiz	'-'
+str_input:   .asciiz	"\nenter integer or operation (+, *): "
+print_char:  .asciiz	"p"
+add_char:    .asciiz	"+"
+space_char:  .asciiz	" "
+mul_char:    .asciiz    "*"
 str_end:     .asciiz	"\nHere is the result: "
 custom_text: .asciiz	"\nyou entered an integer"
 
@@ -13,10 +14,12 @@ custom_text: .asciiz	"\nyou entered an integer"
 	#- $t1 pour l'adresse du string
 	#- $t2 le cpt pour savoir si string fini
 	#- $t3 l'adresse du debut de la stack
-	#- $t4-$t7 pour load des bits et effectuer calculs depuis 
+	#- $t4 pour charger des octets depuis le string
+	#  $t5-$t7 pour load des bits et effectuer calculs depuis 
 	
 	li $t0, 0
-	la $t3, 0($sp)
+	la $t3, ($sp)
+	addi $t3, $t3, 4 # move pointer up for comparison
 		
 	# printing presentation
 pres:
@@ -31,7 +34,7 @@ input:
 	syscall
 
 	la $a0, input_buffer
-	li $a1, 3
+	li $a1, 11
 	li $v0, 8
 	syscall
 	la $t1, input_buffer #load input adress in reg $t1
@@ -42,33 +45,58 @@ load_char:
 	addi $t1, $t1, 1
 	addi $t2, $t2, 1
 	
-	#check if string is done (TODO: change 40)
-	beq $t2, 40, input
+	# check if string is done
+	beq $t2, 10, input
 
-	#check if input == p
+	# check if input == p
 	la $t5, print_char
 	lb $t5, ($t5)
 	beq $t4, $t5, end
 		
-	# if not p, check if add
+	# check if input == +
 	la $t5, add_char
 	lb $t5, ($t5)
 	beq $t4, $t5, addition
+	
+	# check if input == *
+	la $t5, mul_char
+	lb $t5, ($t5)
+	beq $t4, $t5, multiplication
+	
+	# check if input == " "
+	la $t5, space_char
+	lb $t5, ($t5)
+	beq $t4, $t5, load_char
 		
-	# if not add, then transform to int and stack the number
-	# TODO: transform to int
+	# char -> int
+	andi $t4, $t4, 0x0F
+
+	# stack
 	sw $t4, ($sp)
 	sub $sp, $sp, 4
 	j load_char
 		
 addition:
 	# add every stack element to result
-	beq $sp, $t3, end #if stack empty: stop
-	lw $t4, ($sp)
+	beq $sp, $t3, load_char
+	lw $t4, 0($sp)
 	addi $sp, $sp, 4
-	add $t0, $t4, $t0
+	add $t0, $t0, $t4
 	j addition
 	
+multiplication:
+	# multiply every stack element to result
+	beqz $t0, change_starter
+	beq $sp, $t3, load_char
+	lw $t4, 0($sp)
+	addi $sp, $sp, 4
+	mul $t0, $t0, $t4
+	j multiplication
+
+change_starter:
+	# change first result value for multiplication
+	li $t0, 1
+	j multiplication
 end:
 	# print end message
 	la $a0, str_end
